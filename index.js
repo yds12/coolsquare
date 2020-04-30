331afb9f7b36c244ba94c30fcd7ea6f8023bf4d6
yds12 <28710965+yds12@users.noreply.github.com>
2020-04-30 15:16:51 +0200

    Faded color transition working.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+// Setup canvas 
 const canvas = document.getElementById('coolsquare');
 const ctx = canvas.getContext('2d');
 
+const testDiv = document.getElementById('test-div');
+testDiv.style.width = '300px';
+testDiv.style.height = '100px';
+
+// General Constants and Values
 const H = 400;
 const W = 400;
 const TONE_LIM = 40;
-const COLOR = {
-  r: Math.floor(Math.random() * 255),
-  g: Math.floor(Math.random() * 255),
-  b: Math.floor(Math.random() * 255)
-};
+const INTERVAL = 5000;
+const FADE_TIME = 2000;
+const FPS = 10;
+let COLOR = getColor(255, 0, 0);
 
 canvas.width = W;
 canvas.height = H;
@@ -25,7 +30,79 @@ const squares = [
   { x: 300, y: 300, w: 100, h: 100 },
 ];
 
+// Component functions
+function initialize(){
+  for(const sq of squares){
+    sq.color = getTone(COLOR);
+    sq.dColor = getColor(0, 0, 0);
+  }
+
+  setTestDiv(COLOR);
+}
+
+function render(){
+  for(const sq of squares){
+    ctx.fillStyle = `rgb(${sq.color.r},${sq.color.g},${sq.color.b})`;
+    ctx.fillRect(sq.x, sq.y, sq.w, sq.h);
+  }
+}
+
 function divideRecursive(square){
+}
+
+function changeColor(color){
+  setTestDiv(color);
+
+  const deltaTime = 1000 / FPS;
+  const frames = FADE_TIME / deltaTime;
+
+  for(const sq of squares){
+    const tone = getTone(color);
+    sq.dColor.r = (tone.r -sq.color.r) / frames;
+    sq.dColor.g = (tone.g -sq.color.g) / frames;
+    sq.dColor.b = (tone.b -sq.color.b) / frames;
+  }
+
+  let count = 0;
+
+  const si = setInterval(() => {
+    if(count >= frames) {
+      clearInterval(si);
+      return;
+    }
+
+    for(const sq of squares){
+      sq.color.r += sq.dColor.r;
+      sq.color.g += sq.dColor.g;
+      sq.color.b += sq.dColor.b;
+    }
+
+    count++;
+    render();
+  }, deltaTime);
+
+  COLOR = color;
+}
+
+// Utility functions
+function sameColors(color1, color2){
+  if(Math.round(color1.r) === Math.round(color2.r) &&
+     Math.round(color1.g) === Math.round(color2.g) &&
+     Math.round(color1.b) === Math.round(color2.b))
+    return true;
+  return false;
+}
+
+function getColor(r, g, b){
+  return { r: r, g: g, b: b };
+}
+
+function getRandomColor(){
+  return {
+    r: Math.floor(Math.random() * 255),
+    g: Math.floor(Math.random() * 255),
+    b: Math.floor(Math.random() * 255)
+  };
 }
 
 function getToneComponent(color){
@@ -43,15 +120,14 @@ function getTone(color){
   };
 }
 
-function render(){
-  for(sq of squares){
-    const color = getTone(COLOR);
-
-    ctx.fillStyle = `rgb(${color.r},${color.g},${color.b})`;
-    ctx.fillRect(sq.x, sq.y, sq.w, sq.h);
-  }
+function setTestDiv(color){
+  testDiv.style.background = 
+    `rgb(${color.r}, ${color.g}, ${color.b})`;
 }
 
+// Executing
+initialize();
 for(const square of squares) divideRecursive(square);
-
 render();
+//setTimeout(() => changeColor(getColor(0, 0, 255)), INTERVAL);
+setInterval(() => changeColor(getRandomColor()), INTERVAL);
